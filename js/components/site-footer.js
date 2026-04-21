91daf4c51fdff45d77f93f48db752d9346c0bd3d
mog147 <396mog@gmail.com>
2026-04-21 11:42:59 +0900

    add comment

--- js/components/site-footer.js
+++ js/components/site-footer.js
@@ -5,7 +5,7 @@ class SiteFooter extends HTMLElement {
         <span class="footer-logo">Formaldehyde</span>
         <span class="footer-note">
           © Momoko Tezuka &nbsp;·&nbsp;
-          <a href="https://mog147.github.io/" target="_blank" rel="noopener">396 FOLIO →</a>
+          <a href="https://muuu-noir.github.io/mysite/" target="_blank" rel="noopener">396 FOLIO →</a>
         </span>
       </footer>
     `;
