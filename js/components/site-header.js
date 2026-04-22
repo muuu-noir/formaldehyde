dcb7b63f85afbefc64d181a4263238330108f566
mog147 <396mog@gmail.com>
2026-04-23 01:44:24 +0900

    add comment

--- js/components/site-header.js
+++ js/components/site-header.js
@@ -3,7 +3,7 @@ class SiteHeader extends HTMLElement {
     const rootPath = this.getAttribute('root-path') || '.';
     this.innerHTML = `
       <header class="site-header">
-        <a href="${rootPath}/index.html" class="site-logo">F&thinsp;O&thinsp;R&thinsp;M&thinsp;A&thinsp;L&thinsp;D&thinsp;E&thinsp;H&thinsp;Y&thinsp;D&thinsp;E</a>
+        <a href="${rootPath}/room.html" class="site-logo">F&thinsp;O&thinsp;R&thinsp;M&thinsp;A&thinsp;L&thinsp;D&thinsp;E&thinsp;H&thinsp;Y&thinsp;D&thinsp;E</a>
         <nav class="site-nav">
           <a href="${rootPath}/archive.html">Archive</a>
           <a href="${rootPath}/about.html">About</a>
